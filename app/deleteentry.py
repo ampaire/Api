b523d2a12a3cc52f2f85e4b005ed246ad8dea37e
ampaire <42573451+ampaire@users.noreply.github.com>
2018-10-08 22:52:55 +0300

    Update deleteentry.py

--- app/deleteentry.py
+++ app/deleteentry.py
@@ -7,19 +7,19 @@ entries = [
     {
         'id_': 1,
         'date': 28/7/2018,
-        'Topic': "My Birthday",
-        'Contents': "I had actually forgotten all about this day being my birthday." +
-        "I got out of my room and all I got was a shower of my life." +
-        "All my friends stood by the door with satisfied smiles watching me dripping wet."
+        'Topic': "My Birthday!",
+        'Contents': "I had actually forgotten all about this day being my birthday..." +
+        "I got out of my room and all I got was a shower of my life!" +
+        "All my friends stood by the door with satisfied smiles watching me dripping wet. Wooow"
     },
     {
         'id_': 2,
         'date': 29/7/2018,
-        'Topic': "Rachel's visit",
-        'Contents': "Dear Diary, did I tell you about Rachel?. Racheal is my elder sister." +
-        "She is the picky type and loves good food. So I spent the whole morning cooking" +
-        "I had to literally scrub the whole house plus the ceiling... haha" +
-        "She is the tidy one you know and strict hehe I mean strict about hygiene."
+        'Topic': "Rachel's visit!",
+        'Contents': "Dear Diary, did I tell you about Rachel?... Racheal is my elder sister," +
+        "She is the picky type and loves good food. I spent the whole morning cooking," +
+        "Ieven had to scrub the whole house plus the ceiling... haha" +
+        "She is the tidy one you know.. and strict hehehe.. I mean strict about hygiene."
     }
 ]
 
